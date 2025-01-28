a2b376a2014c535b36965ce2c2d8787adc43f78f
puru-aws <94843505+puru-aws@users.noreply.github.com>
2025-01-28 09:39:34 +0000

    Adding scripts

--- scripts/install_dependancy.sh
+++ scripts/install_dependancy.sh
@@ -1,6 +1,7 @@
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 yum install git -y
+cd /home/ssm-user
 git clone https://github.com/puru-aws/sample-app-e2e-testing.git
 cd sample-app-e2e-testing
 python3 -m pip install -r requirements.txt
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,1 +1,2 @@
+cd /home/ssm-user/sample-app-e2e-testing
 uvicorn app.main:app --host 0.0.0.0 --port 8000
