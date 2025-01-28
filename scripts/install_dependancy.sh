b4770f7a24162c9a1f7a082abce5b6fe5c97d1e7
puru-aws <94843505+puru-aws@users.noreply.github.com>
2025-01-28 09:36:42 +0000

    Adding scripts

--- scripts/install_dependancy.sh
+++ scripts/install_dependancy.sh
@@ -1,5 +1,5 @@
 curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
+python3 get-pip.py --user
 yum install git -y
 git clone https://github.com/puru-aws/sample-app-e2e-testing.git
 cd sample-app-e2e-testing
